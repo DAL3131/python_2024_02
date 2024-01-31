45fb95395230675fb0dd42c374c4103ac0969fec
DAL3131 <89055703+DAL3131@users.noreply.github.com>
2024-01-31 10:49:28 +0900

    Add files via upload

---
+++ day01/var.py
@@ -0,0 +1,35 @@
+#var
+#variable - 변수
+
+# 10 + 50 [연산 + 기억]
+# 10 + 50 [연산]
+# number = 10 + 50 [연산 + 기억]
+# myname = input("너의 이름은?")
+# # 우변을 좌변으로 입력
+# print(myname)
+# age = 10+4
+# print(age)
+
+
+
+
+
+
+# 1. 당신의 이름?
+# 2. 당신의 나이?
+# 3. 당신의 최애 음식
+# 4. 당신의 최애 유튜버는?
+
+name = input("당신의 이름?")
+age = input("당신의 나이?")
+favorite_food = input("당신의 최애 음식")
+FavoriteYt = input("당신의 최애 유튜버는?")
+print(name)
+print(age)
+print(food)
+print(yt)
+
+
+
+
+
