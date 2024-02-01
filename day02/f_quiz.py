2b66c58debc660eeba85170480fec0a6171735a8
DAL3131 <89055703+DAL3131@users.noreply.github.com>
2024-02-01 10:45:45 +0900

    Add files via upload

---
+++ day02/f_quiz.py
@@ -0,0 +1,37 @@
+# 1. 당신의 최애 음식은:
+# 2. 당신의 최애 동물은:
+# 3. 당신의 mbti는:
+# 4. 당신의 사는 곳은:
+
+
+
+# -> 당신이 좋아하는 음식은 ~~ 이고, 최애 동물은 ~~ 이군요!
+# -> 당신의 mbti는 ~~ 이시고, 사는 곳은 ~~이시군요!
+
+# favoriteFood = input("당신의 최애 음식은:")
+# favoriteAnimal = input("당신의 최애 동물은:")
+# mbti = input("당신의 mbti는:")
+# home = input("당신의 사는 곳은:")
+#
+# print(f"당신이 좋아하는 음식은 {favoriteFood}이고, 최애 동물은 {favoriteAnimal}이군요!")
+# print(f"당신의 mbti는 {mbti}이시고, 사는 곳은 {home}이시군요!")
+
+# 첫 번째 숫자 입력:
+# 두 번째 숫자 입력:
+# 두 수의 합은 ~~ 입니다.
+
+number1 = input("첫 번째 숫자 입력:")
+number2 = input("두 번째 숫자 입력:")
+
+print(f"두 수의 합은 {number1 + number2}입니다.")
+
+
+
+
+
+
+
+
+
+
+
---
+++ day02/review.py
@@ -0,0 +1,11 @@
+time = input("오늘의 기상 시간은?")
+breakfast = input("오늘의 아침 메뉴는?")
+work = input("오늘의 출근시간은?")
+
+print(time)
+print(breakfast)
+print(work)
+
+
+
+
