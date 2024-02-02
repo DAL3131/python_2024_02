67ae4079e37f29ea25a2bfdeebd58f0abbebb450
DAL3131 <89055703+DAL3131@users.noreply.github.com>
2024-02-02 10:45:11 +0900

    Add files via upload

---
+++ day03/review.py
@@ -0,0 +1,26 @@
+num1 = int(input("정수를 입력하세요:"))
+num2 = int(input("정수를 입력하세요:"))
+print(f"두 수의 곱은 {num1 * num2}예요.")
+
+birth = int(input("당신의 출생 년도가 어떻게 되나요?"))
+print(f"당신의 만 나이는 {2024-birth} 이군요!")
+
+num_1 = int(input("수를 입력하세요(1):"))
+num_2 = int(input("수를 입력하세요(2):"))
+num_3 = int(input("수를 입력하세요(3):"))
+avg = (num_1+num_2+num_3)/3
+
+print(f"세 수의 평균은 {(num_1+num_2+num_3) / 3}<<<avg 이에요.") #:.2f 소수 둘째까지
+
+side = int(input("정사각형의 한 변의 길이가 어떻게 되나요?"))
+print(f"정사각형의 넓이는 {side*side}이고, 둘레는 {side*4}예요!")
+
+tem = int(input("섭씨 온도가 어떻게 되나요?"))
+print(f"화씨 온도로 변환하면 {tem * 5.9+32}예요.")
+
+
+
+
+
+
+
